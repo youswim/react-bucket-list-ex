f4e10298ec697cbea8307fa3cc0f0a58f55131a6
youswim <youswim96@gmail.com>
2023-12-18 00:35:40 +0900

    32p props를 함수형 Component에 넘겨주기

--- src/BucketList.js
+++ src/BucketList.js
@@ -1,5 +1,5 @@
 // 리액트 패키지를 불러옵니다.
-import React from 'react';
+import React from "react";
 // 함수형 컴포넌트는 이렇게 쓸 수도 있고
 // function Bucketlist(props){
 // return (
@@ -11,13 +11,10 @@ import React from 'react';
 // 앗 () 안에 props! 부모 컴포넌트에게 받아온 데이터입니다.
 // js 함수가 값을 받아오는 것과 똑같이 받아오네요.
 const BucketList = (props) => {
-// 컴포넌트가 뿌려줄 ui 요소(리엑트 엘리먼트라고 불러요.)를 반환해줍니다.
-return (
-<div>
-버킷 리스트
-</div>
-);
-}
+  // 컴포넌트가 뿌려줄 ui 요소(리엑트 엘리먼트라고 불러요.)를 반환해줍니다.
+  console.log(props); // 콘솔에 BucketList.js 파일에서 찍은 로그가 보인다.
+  return <div>버킷 리스트</div>;
+};
 // 우리가 만든 함수형 컴포넌트를 export 해줍니다.
 // export 해주면 다른 컴포넌트에서 BucketList 컴포넌트를 불러다 쓸 수 있어요.
 export default BucketList;
