d23d9caa8ef3bac46c62dd66ea016a4dfbcdb3aa
youswim <youswim96@gmail.com>
2023-12-31 00:51:17 +0900

    styled-component 적용

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import logo from "./logo.svg";
 import BucketList from "./BucketList";
 // import "./style.css";
 import "./scss_ex.scss";
+import styled from "styled-components";
 
 // 클래스형 컴포넌트는 이렇게 생겼습니다!
 class App extends React.Component {
@@ -21,15 +22,44 @@ class App extends React.Component {
     console.log(this.state);
     return (
       <div className="App">
-        <div className="container">
-          <h1 className="title">내 버킷리스트</h1>
-          <hr className="line" />
+        <MyContainer>
+          <MyTitle >내 버킷리스트</MyTitle>
+          <MyLine/>
           {/* 컴포넌트를 넣어줍니다. */}
           {/* <컴포넌트 명 [props 명]={넘겨줄 것(리스트, 문자열, 숫자, ...)}/> */}
           <BucketList list={this.state.list} />
-        </div>
+        </MyContainer>
       </div>
     );
   }
 }
+
+const MyApp = styled.div`
+  width: 100vw;
+  min-height: 100vh;
+  background-color: #eee;
+  padding: 32px;
+  box-sizing: border-box;
+`;
+
+const MyContainer = styled.div`
+  max-width: 350px;
+  min-height: 80vh;
+  background-color: #fff;
+  padding: 16px;
+  margin: 20px auto;
+  border-radius: 5px;
+  border: 1px solid #ddd;
+`;
+
+const MyTitle = styled.h1`
+  color: slateblue;
+  text-align: center;
+`;
+
+const MyLine = styled.hr`
+  margin: 16px 0px;
+  border: 1px dotted #ddd;
+`;
+
 export default App;
