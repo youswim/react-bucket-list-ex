ec5b771d181d995a0d61cdf620ab17f5e7638395
youswim <youswim96@gmail.com>
2024-01-21 21:59:50 +0900

    리덕스 훅 사용
    부모 컴포넌트로부터 데이터를 props 형태로 받아오던 BucketList를 리덕스 스토어에서 useSelector를 사용하여 데이터를 가져오도록 변경

--- src/BucketList.js
+++ src/BucketList.js
@@ -2,6 +2,7 @@
 import React from "react";
 import styled from "styled-components";
 import { useNavigate } from "react-router-dom";
+import { useDispatch, useSelector } from "react-redux";
 // 함수형 컴포넌트는 이렇게 쓸 수도 있고
 // function Bucketlist(props){
 // return (
@@ -13,6 +14,9 @@ import { useNavigate } from "react-router-dom";
 // 앗 () 안에 props! 부모 컴포넌트에게 받아온 데이터입니다.
 // js 함수가 값을 받아오는 것과 똑같이 받아오네요.
 const BucketList = (props) => {
+
+  const bucket_list = useSelector(state => state.bucket.list);
+
   // Quiz 1: my_list에 ['a', 'b', 'c'] 대신 부모 컴포넌트가 넘겨준 값을 넣으려면 어떻게 해야할까요?
   const my_lists = props.list;
   const navigate = useNavigate();
@@ -28,7 +32,7 @@ const BucketList = (props) => {
         // js의 내장 함수 중 하나인 map입니다. 리스트의 갯수만큼 => 오른쪽 구문을 반복해요.
         // 자세한 사용법은 아래 링크를 확인해주세요.
         // https://developer.mozilla.org/ko/docs/Web/JavaScript/Reference/Global_Objects/Array/map
-        my_lists.map((list, index) => {
+        bucket_list.map((list, index) => {
           // 콘솔을 확인해봅시다 :)
           console.log(list);
           return (
